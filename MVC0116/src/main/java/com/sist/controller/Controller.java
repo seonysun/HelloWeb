8fffcdb7a6ba759425749001db4ec014e67eecf6
user <user@DESKTOP-COJ9858>
2023-01-19 09:42:40 +0900

    task_mvc 컨트롤러

---
+++ MVC0116/src/main/java/com/sist/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.sist.controller;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE) //클래스 구분자
+public @interface Controller {
+
+}
